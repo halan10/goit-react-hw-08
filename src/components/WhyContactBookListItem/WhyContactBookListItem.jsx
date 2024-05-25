da5b64ffb4bf91a36bfe052faaac7cba057e4e16
halan10 <halanmykola@gmail.com>
2024-05-25 22:42:47 +0300

    fix 'why contact book' component background

--- src/components/WhyContactBookListItem/WhyContactBookListItem.jsx
+++ src/components/WhyContactBookListItem/WhyContactBookListItem.jsx
@@ -1,13 +1,11 @@
-import Paper from '@mui/material/Paper';
-
 import css from './WhyContactBookListItem.module.css';
 
 export default function WhyContactBookListItem({ item }) {
   return (
-    <Paper className={css.paper}>
+    <div className={css.paper}>
       <img src={item.icon} alt="Avatar" width="148" />
       <h3>{item.title}</h3>
       <p className={css.description}>{item.description}</p>
-    </Paper>
+    </div>
   );
 }
